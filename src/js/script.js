e0eb380ae172c1468895ceb02df345b50f533153
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2025-04-11 19:40:26 -0300

    refactor: troca do banco de dados local para o headless CMS ContentFul

--- src/js/script.js
+++ src/js/script.js
@@ -54,90 +54,109 @@ const btnAll = document.querySelector('#all');
 const btnFrelance = document.querySelector('#freelance');
 const btnPessoal = document.querySelector('#pessoal');
 
-// requisicao ao banco de dados local
-async function dataBase() {
-  const response = await fetch('src/dataBase/dataBase.json');
-  const data = await response.json();
-  const arrData = [...data];
-
-  return arrData;
-};
-
 // funcao que monta os cards de projeto
-function cardProject(img, type, technologies, title, linkSite, arialLabelSite, linkGithub, arialLabelGithub) {
-  let html = `<div class="c-main__cardProject">
+function cardProject(img, alt, type, title, technologies, linkSite, linkGithub) {
+  let html = `
+    <div class="c-main__cardProject">
       <div class="c-main__preview">
-        <img src="${img}" alt="">
+        <img src="${img}" alt=${alt}>
       </div>
 
-        <span class="c-main__category">${type}</span>
+      <span class="c-main__category">${type}</span>
 
-        <div>${technologies.join('')}</div>
+      <h3 class="c-main__titleProject">${title}</h3>
 
-        <h3 class="c-main__titleProject">${title}</h3>
+      <div class="c-main__technologies">
+        ${technologies}
+      </div>
 
-        <div class="c-main__links">
-          <a href="${linkSite}" target="_blank" aria-label="${arialLabelSite}"><i class="fa-regular fa-regular fa-eye"></i></a>
-            
-          <a href="${linkGithub}" target="_blank" aria-label="${arialLabelGithub}"><i class="fa-brands fa-github"></i></a>
-        </div>
-    </div>`;
+      <div class="c-main__links">
+        <a href="${linkSite}" target="_blank" aria-label="link-do-site"><i class="fa-regular fa-regular fa-eye"></i></a>
+          
+        <a href="${linkGithub}" target="_blank" aria-label="link-do-repositório"><i class="fa-brands fa-github"></i></a>
+      </div>
+    </div>
+  `;
 
   return html;
 };
 
-// faz a renderizacao dos cards de projeto
-dataBase()
-  .then(dados => {
-    btnAll.addEventListener('click', () => {
-    listProjects.innerHTML = "";
+// requisicao ao headless CMS
+const cardProduct = document.querySelector('.cardProduct');
+const client = contentful.createClient({
+  space: 'xgtiddt5x05g',
+  environment: 'master',
+  accessToken: '6WgSiu0DVitqSf-aKTzdRgtrccPr1MfVBjRoyI6FH8E'
+})
+client.getEntries({
+  content_type: 'projectPortifolio'
+})
+.then((response) => {
+
+  const arr = response.items
+  const pessoal = arr.filter(item => item.fields.typeProject === "PESSOAL");
+  const freelance = arr.filter(item => item.fields.typeProject === "FREELANCE");  
+
+  function renderProjects(array) {
+    
+    array.forEach(element => {
+      
+      const imgPreview = element.fields.imgPreview.fields.file.url;
+      const imgAlt = element.fields.imgPreview.fields.title;
+      const typeProject = element.fields.typeProject;
+      const titleProject = element.fields.title;
+      const linkSite = element.fields.linkSite;
+      const linkGithub = element.fields.linkGithub;
+      const arrTechnologies = element.fields.technologies;
+      let technologies = "";
+
+      for (let i = 0; i < arrTechnologies.length; i++) {
+        technologies += `<img width='30' height='30' src=${arrTechnologies[i].fields.file.url} alt=${arrTechnologies[i].fields.title}/>`
+        
+      }
+  
+      listProjects.innerHTML += cardProject(imgPreview, imgAlt, typeProject, titleProject, technologies, linkSite, linkGithub)
+      
+    });
+  }
+
+  listProjects.innerHTML = "";
+  renderProjects(arr);
+
+  btnAll.addEventListener('click', () => {
+
     btnAll.classList.add('active');
     btnFrelance.classList.remove('active');
     btnPessoal.classList.remove('active');
 
-    dados.forEach((item) => {
-      listProjects.innerHTML += 
-        cardProject(item.img, item.type, item.technologies, item.title, item.linkSite, item.arialLabelSite, item.linkGithub, item.arialLabelGithub)
-
-      });
-    });
-
-    btnAll.click();
-
-    btnFrelance.addEventListener('click', () => {
     listProjects.innerHTML = "";
+
+    renderProjects(arr);
+
+  });
+
+  btnFrelance.addEventListener('click', () => {
+
     btnFrelance.classList.add('active');
     btnAll.classList.remove('active');
     btnPessoal.classList.remove('active');
 
-    const freelancesArr = dados.filter((item) => {
-      return item.type === "FREELANCE";
-    });
+    listProjects.innerHTML = "";
 
-    freelancesArr.forEach((item) => {
-      listProjects.innerHTML += 
-        cardProject(item.img, item.type, item.technologies, item.title, item.linkSite, item.arialLabelSite, item.linkGithub, item.arialLabelGithub)
+    renderProjects(freelance)
 
-      });
-    });
-
-    btnPessoal.addEventListener('click', () => {
-      listProjects.innerHTML = "";
-      btnPessoal.classList.add('active');
-      btnAll.classList.remove('active');
-      btnFrelance.classList.remove('active');
-
-      const pessoalArr = dados.filter((item) => {
-        return item.type === "PESSOAL";
-      });
-
-      pessoalArr.forEach((item) => {
-        listProjects.innerHTML += 
-          cardProject(item.img, item.type, item.technologies, item.title, item.linkSite, item.arialLabelSite, item.linkGithub, item.arialLabelGithub)
-
-      });
-    });
-  })
-  .catch(error => {
-    console.error(error);
   });
+
+  btnPessoal.addEventListener('click', () => {
+
+    btnPessoal.classList.add('active');
+    btnAll.classList.remove('active');
+    btnFrelance.classList.remove('active');
+
+    listProjects.innerHTML = "";
+
+    renderProjects(pessoal)
+
+  });
+
+});
